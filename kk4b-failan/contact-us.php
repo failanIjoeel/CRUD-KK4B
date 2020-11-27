464fafd72e9e1e0dfeec37369aa24b8bdef33576
failanIjoeel <69224688+failanIjoeel@users.noreply.github.com>
2020-11-27 21:37:20 +0700

    Add files via upload

---
+++ kk4b-failan/contact-us.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Order Now</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome!</title>
+    <link rel="stylesheet" type="text/css" href="css/style2.css">
+    <link href="https://fonts.googleapis.com/css?family=Amaranth" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+
+  </head>
+  <body>
+
+    <header style="background: #484848;">
+      <div class="container">
+        <h1><a>iJoel</a></h1>
+        <ul>
+          <li><a href="index.php">About</a></li>
+          <li><a href="produk.php">Menu</a></li>
+          <li><a href="contact-us.php">Oder Now</a></li>
+        </ul>
+      </div>
+    </header>
+
+    <div class="container">
+      <link rel="stylesheet" type="text/css" href="css/stylecontact.css">
+      <form id="contact" action="" method="post">
+        <h3>Your Order</h3>
+        <fieldset>
+          <input placeholder="Your Name" type="text" tabindex="1" required autofocus>
+        </fieldset>
+        <fieldset>
+          <input placeholder="Your Address" type="text" tabindex="2" required>
+        </fieldset>
+        <fieldset>
+          <input placeholder="Your Phone Number" type="text" tabindex="3" required>
+        </fieldset>
+
+        <fieldset>
+          <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+        </fieldset>
+      </form>
+    </div>
+       <footer>
+         <div class="container">
+           <small>Copyright &copy; 2020 - Failan Rahsya Izzul Pratama</small>
+         </div>
+       </footer>
+   </body>
+</html>
